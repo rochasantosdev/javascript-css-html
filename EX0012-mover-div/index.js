0c1313bd5e83e63c72dce77b32d471891ac4f650
rochadev <rochasantosvideos@gmail.com>
2024-02-14 20:51:20 -0300

    Commit

--- EX0012-mover-div/index.js
+++ EX0012-mover-div/index.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("botão");
 const div = document.getElementById("caixa");
 
-console.log(btn)
-console.log(div)
+function minhaFuncao() {
+    alert("Botão clicado!");
+  }
